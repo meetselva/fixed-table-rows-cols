303f2fef5c0e746fa7c812670b1e2f71a76062d2
meetselva <meetselva@gmail.com>
2013-05-31 15:16:29 -0400

    MIT License

--- fixed_table_rc.js
+++ fixed_table_rc.js
@@ -2,20 +2,10 @@
 A jQuery plugin to convert a well formatted table into a table with fixed
 rows and columns.
 
-Copyright (C) (2011-2012) Selvakumar Arumugam
-
-This program is free software: you  can redistribute it and/or modify it
-under the  terms of the GNU  General Public License as  published by the
-Free Software Foundation,  either version 3 of the License,  or (at your
-option) any later version.
-
-This  program  is distributed  in  the  hope  that  it will  be  useful,
-but  WITHOUT  ANY  WARRANTY;  without   even  the  implied  warranty  of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General
-Public License for more details.
-
-You should have received a copy  of the GNU General Public License along
-with this program. If not, see <http://www.gnu.org/licenses/>.
+About License:
+Copyright (C) 2013 Selvakumar Arumugam
+You may use attrchange plugin under the terms of the MIT Licese.
+https://github.com/meetselva/fixed-table-rows-cols/blob/master/MIT-License.txt
 */
 (function ($) {
 	
